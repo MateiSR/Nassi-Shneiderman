14f4523f8285081ab299fb5abf21e115203598da
KirbyPast <iovalex29@gmail.com>
2023-12-04 21:34:07 +0200

    Bazele analizei pseudocodului
    Am creat un enum care sa acopere majoritate tipurilor de linii din pseudocod, m-am luat dupa fisa de la sd cand a venit vorba de sintaxa de la do while, ca era oleaca ambiguu intre while si do while.
    Am implementat citirea basic linie cu linie a codului si o verificare a tipului de linie. Important e ca acoladele sa fie pe linii separate, singurem ca sa ne fie viata mult mai usoara. pwp.

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,24 +1,60 @@
 #include <iostream>
 #include <graphics.h>
 #include <winbgim.h>
-/*
-while (x) do
+#include <stdio.h>
+using namespace std;
+
+enum LineType
 {
-cod
+    otherStatement = 0,
+    ifStatement = 1,
+    elseStatement = 2,
+    whileStatement = 3,
+    repeatUntilStatementBegin = 4,
+    repeatUntilStatementEnd = 5,
+    forStatement = 6,
+    braceBeggining = 7,
+    braceEnd = 8
+} lineType;
+
+
+LineType getLineType(char codeLine[])
+{
+    if(strstr(codeLine,"if")) return ifStatement;
+    else if(strstr(codeLine,"else")) return elseStatement;
+    else if(strstr(codeLine,"while")) return whileStatement;
+    else if(strstr(codeLine,"repeat")) return repeatUntilStatementBegin;
+    else if(strstr(codeLine,"until")) return repeatUntilStatementEnd;
+    else if(strstr(codeLine,"for")) return forStatement;
+    else if(strstr(codeLine,"{")) return braceBeggining;
+    else if(strstr(codeLine,"}")) return braceEnd;
+    else return otherStatement;
 }
-*/
+
+
+void analyzeCode(FILE *fptr, char rawCode[])
+{
+    while(fgets(rawCode,61,fptr)){
+        lineType=getLineType(rawCode);
+        cout<<lineType<<endl;
+    }
+
+}
+
 
 int main()
 {
+    char rawCode[61];
     FILE *pseudocode = fopen("input.txt", "r");
-    if (!pseudocode) { printf("Could not open file"); return -1; }
-    /*
-//  int gd = DETECT, gm;
-//  initgraph(&gd, &gm, "");
-  initwindow(800,600); int x = 400, y = 300, r;
-  for (r = 25; r <= 125; r += 20)
-      circle(x, y, r);
-  getch(); closegraph(); return 0;
-*/
+    if (!pseudocode)
+    {
+        printf("Could not open file");
+        return -1;
+    }
+    else
+    {
+        analyzeCode(pseudocode,rawCode);
+    }
+
     return 0;
 }
