6b517e570a50bca90bfd238d404c14546051ea1e
Matei <mateisr@proton.me>
2023-12-04 20:14:39 +0200

    Add header files

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,24 +1,30 @@
-#include <iostream>
+/* Libraries */
+#include <stdio.h>
 #include <graphics.h>
 #include <winbgim.h>
-/*
-while (x) do
-{
-cod
-}
-*/
+#include <string.h>
+
+/* Header files */
+#include <UI.h>
+
+/* Constant variables */
+const int maxHeight = getmaxheight() * 0.75, maxWidth = getmaxwidth() * 0.75;
+
+/* Function headers */
+void run();
+
 
 int main()
 {
     FILE *pseudocode = fopen("input.txt", "r");
     if (!pseudocode) { printf("Could not open file"); return -1; }
-    /*
-//  int gd = DETECT, gm;
-//  initgraph(&gd, &gm, "");
-  initwindow(800,600); int x = 400, y = 300, r;
-  for (r = 25; r <= 125; r += 20)
-      circle(x, y, r);
-  getch(); closegraph(); return 0;
-*/
+
+    // Run process
+    run();
+
     return 0;
+}
+
+void run() {
+    createWindow();
 }
