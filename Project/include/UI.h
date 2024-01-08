4aeaf61149872da86f03839c6e57904975afce24
KirbyPast <iovalex29@gmail.com>
2024-01-08 16:24:57 +0200

    culori de fundal

--- Project/include/UI.h
+++ Project/include/UI.h
@@ -9,7 +9,7 @@
 #pragma once
 
 // Variables
-const int WIDTH = 900, HEIGHT = 900;
+const int WIDTH = 1200, HEIGHT = 900;
 const int MAX_WIDTH = WIDTH * 0.95, MAX_HEIGHT = HEIGHT * 0.95;
 int DIAGRAM_SIZE = 0;
 
@@ -28,7 +28,8 @@ const int DEFAULT_COLOR = COLOR(255,255,255),
     TEST_BEFORE_COLOR = COLOR(255,255,200),
     TEST_AFTER_COLOR = COLOR(200,200,0),
     FOR_COLOR = COLOR(173,216,230),
-    SIMPLE_COLOR = COLOR(255,255,255);
+    SIMPLE_COLOR = COLOR(200,200,200);
+const int BG_COLOR = 10;
 
 const int getBlockColor(int lineType) {
     if (lineType == 0) return SIMPLE_COLOR;
@@ -110,7 +111,6 @@ void outTextMiddle(int y, char* s) {
     outtextxy(WIDTH/2, y, s);
 }
 
-const int BG_COLOR = COLOR(45,45,45);
 
 void generateWindowContent() {
 
@@ -129,68 +129,25 @@ void generateWindowContent() {
 // Draw simple instruction block
 void drawSimpleBlock(block Block, int &top, int &left, int right) {
     int originalColor = getcolor();
-    setcolor(getBlockColor(Block.lineType));
     int blockSize = textheight(Block.rawLine);
     int bottom = top + blockSize;
+
     printf("!%d\n", bottom);
     line(left, bottom + SPACE_UNDER_TEXT, right, bottom + SPACE_UNDER_TEXT);
-    rectangle(left, top, right, bottom + SPACE_UNDER_TEXT);
+    if (Block.lineType == 0) {
+        setfillstyle(SOLID_FILL, SIMPLE_COLOR);
+        bar(left, top, right, bottom + SPACE_UNDER_TEXT);
+        setbkcolor(SIMPLE_COLOR);
+        rectangle(left, top, right, bottom + SPACE_UNDER_TEXT);
+    }
+    else {
+        setbkcolor(TEST_AFTER_COLOR);
+        line (left, bottom + SPACE_UNDER_TEXT, right, bottom + SPACE_UNDER_TEXT);
+    }
     outtextxy((left + right)/2, bottom, Block.rawInstruction);
     top = bottom + SPACE_UNDER_TEXT;
+    setfillstyle(SOLID_FILL, BG_COLOR);
     printf("showing text: %s at %d, block id %d, y=%d\n", Block.rawInstruction, bottom, Block.index, top);
-    setcolor(originalColor);
-}
-
-
-// Draw if block start in NS diagram
-void drawIfStartBlock(char *condition, int top, int left, int blockSize = 50) {
-    int bottom = top + blockSize,
-        right = MAX_WIDTH;
-    rectangle(left, top, right, bottom);
-    int center = (left+right)/2;
-    line(left, top, center, bottom);
-    line(right, top, center, bottom);
-
-    outtextxy(left + textwidth("TRUE") / 2 + 10, bottom - 10, "TRUE");
-    outtextxy(right - textwidth("FALSE") / 2 - 10, bottom - 10, "FALSE");
-    outtextxy(center, top + textheight(condition), condition);
-
-}
-
-// Draw the parallel block of the if
-void drawIfParallelBlock(block Block, int top, int left, int blockSize = 200, bool showText = false) {
-    int bottom = top + blockSize,
-        right = MAX_WIDTH;
-    rectangle(left, top, right, bottom);
-    int center = (left+right)/2;
-    line(center, top, center, bottom);
-    if (showText) showCodeFromBlock(Block, left, top,left);
-    // Find next else block
-    bool foundElse = false;
-    int i = Block.index + 1;
-    while (!foundElse) {
-        if (blockVector.Block[i].lineType == 2) {
-            foundElse = true;
-            block elseBlock = blockVector.Block[i];
-            if (showText) showCodeFromBlock(elseBlock, center, top,left);
-        }
-            i++;
-    }
-    //printf("----%d----\n", foundElse);
-}
-
-// Draw the parallel block of the else
-void drawElseParallelBlock(block Block, int top, int left, int blockSize = 200, bool showText = false) {
-
-}
-
-// Draw the full if block
-void drawIfBlock(block Block, int top, int left, int blockSize = 200, bool showText = false) {
-    char* condition = Block.rawInstruction;
-    const int ifBlockSize = max((int)(blockSize / 3.5), 50);
-
-    drawIfStartBlock(condition, top, left, ifBlockSize);
-    drawIfParallelBlock(Block, top + ifBlockSize, left, blockSize, showText);
 }
 
 // --- For, while, do while (<=> repeat until !) ---
@@ -199,8 +156,9 @@ void drawIfBlock(block Block, int top, int left, int blockSize = 200, bool showT
 int drawForLoop(block Block, int& top, int& left, int right) {
     int blockHeight = getBlockSize(Block);
     int bottom = top + blockHeight;
-    int originalColor = getcolor();
-    setcolor(getBlockColor(Block.lineType));
+    setfillstyle(SOLID_FILL, FOR_COLOR);
+    bar(left, top, right, bottom);
+    setbkcolor(FOR_COLOR);
     printf("drawing for: bottom is %d\n", bottom);
 
     rectangle(left, top, right, bottom);
@@ -209,15 +167,17 @@ int drawForLoop(block Block, int& top, int& left, int right) {
     bottom -= textheight(Block.rawLine) + SPACE_UNDER_TEXT;
     rectangle(left, top, right, bottom);
     outtextxy((left + right) / 2, top - SPACE_UNDER_TEXT, Block.rawInstruction);
-    setcolor(originalColor);
+    setfillstyle(SOLID_FILL, BG_COLOR);
+    setbkcolor(BG_COLOR);
     return bottom;
 }
 
 void drawLoopTestBefore(block Block, int &top, int &left, int right) {
     int blockHeight = getBlockSize(Block);
     int bottom = top + blockHeight;
-    int originalColor = getcolor();
-    setcolor(getBlockColor(Block.lineType));
+    setfillstyle(SOLID_FILL, TEST_BEFORE_COLOR);
+    bar(left, top, right, bottom);
+    setbkcolor(TEST_BEFORE_COLOR);
     printf("drawing while: bottom is %d\n", bottom);
     rectangle(left, top, right, bottom);
 
@@ -228,17 +188,21 @@ void drawLoopTestBefore(block Block, int &top, int &left, int right) {
     top += textheight(Block.rawInstruction) + SPACE_UNDER_TEXT;
 
     rectangle(left, top, right, bottom);
-    setcolor(originalColor);
+
+    setfillstyle(SOLID_FILL, BG_COLOR);
+    setbkcolor(BG_COLOR);
 }
 
 
 int drawLoopTestAfter(block Block, int& top, int& left, int right) {
-    int originalColor = getcolor();
-    setcolor(getBlockColor(Block.lineType));
     int blockHeight = getBlockSize(Block);
     // calculam bottom pt dreptunghiul MIC!! (si leftul tot ptr ala)
     int bottom = top + blockHeight - (textheight(Block.rawLine) + SPACE_UNDER_TEXT);
     left += textheight(Block.rawLine);
+
+    setfillstyle(SOLID_FILL, TEST_AFTER_COLOR);
+    bar(left, top, right, bottom);
+    setbkcolor(TEST_AFTER_COLOR);
     rectangle(left, top, right, bottom); cout << "am desenat\n";
 
     //apoi calculam bottom si left pt dreptunghiul ala mare !!
@@ -249,7 +213,9 @@ int drawLoopTestAfter(block Block, int& top, int& left, int right) {
     // si la sfarsit le ducem inapoi la valoarea de la dreptunghiul mic ca sa ramana left-ul cum trb si sa mearga si bottom.
     left += textheight(Block.rawLine);
     bottom -= textheight(Block.rawLine) + SPACE_UNDER_TEXT;
-    setcolor(originalColor);
+
+    setfillstyle(SOLID_FILL, BG_COLOR);
+    setbkcolor(BG_COLOR);
     return bottom;
 }
 
@@ -258,46 +224,21 @@ void drawDiagramBorder() {
     int bottom = MAX_HEIGHT,
     left = MAX_WIDTH * 0.05,
     top = MAX_HEIGHT * 0.05,
-    right = MAX_WIDTH;
+    right = MAX_WIDTH * 0.95;
     printf("Diagram borders: %d %d %d %d \n", left, top, right, bottom);
     rectangle(left, top, right, bottom);
 }
 
-// Get child coordinates
-/*
-void getChildCoords(block Block, int &top, int &left, int &right, int blockSize) {
-    char *condition = Block.rawInstruction;
-    right = MAX_WIDTH;
-    if (Block.lineType == 3) {
-        top += textheight(condition);
-        left += textheight(condition);
-    }
-    else if (Block.lineType == 4) {
-        left += textheight(condition);
-        //bottom -= textheight(condition);
-    }
-    else if (Block.lineType == 6) {
-        left += textheight(condition);
-        //bottom -= textheight(condition);
-        top += textheight(condition);
-    }
-    else if (Block.lineType == 0) {
-        top += textheight(Block.rawInstruction);
-    }
-    else if (Block.lineType == 1) {
-        top += max((int)(blockSize / 3.5), 50);
-        // daca e intr-un if
-        right = (left+right)/2;
-    }
-    else if (Block.lineType == 2) {
-        top += textheight(condition);
-    }
-}
-*/
+
 
 void drawIf(block Block, int &top, int &left, int &right) {
     int blockHeight = getBlockSize(Block);
     int bottom = top + (textheight(Block.rawLine) + SPACE_UNDER_TEXT) * 2;
+
+    setfillstyle(SOLID_FILL, IF_COLOR);
+    bar(left, top, right, bottom);
+    setbkcolor(IF_COLOR);
+
     rectangle(left, top, right, bottom);
     line(left, top, (left + right) / 2, bottom);
     line(right, top, (left + right) / 2, bottom);
@@ -310,6 +251,17 @@ void drawIf(block Block, int &top, int &left, int &right) {
 
     right = (left + right) / 2;
     top = bottom;
+
+    setfillstyle(SOLID_FILL, BG_COLOR);
+    setbkcolor(BG_COLOR);
+}
+
+void drawLoopTestAfterRectangle(block Block, int top, int left, int right) {
+    int bottom = top + getBlockSize(Block);
+    setfillstyle(SOLID_FILL, TEST_AFTER_COLOR);
+    bar(left, top, right, bottom);
+    rectangle(left, top, right, bottom);
+    setfillstyle(SOLID_FILL, BG_COLOR);
 }
 
 void createDiagram(blockChain blockVector, int currTop = MAX_HEIGHT * 0.05) {
@@ -318,7 +270,7 @@ void createDiagram(blockChain blockVector, int currTop = MAX_HEIGHT * 0.05) {
     int diagramRight = MAX_WIDTH * 0.95;
 
     int currLeft = diagramLeft;
-    int currRight = MAX_WIDTH;
+    int currRight = diagramRight;
     int currBottom;
 
     int lastPriority = -1;
@@ -338,6 +290,9 @@ void createDiagram(blockChain blockVector, int currTop = MAX_HEIGHT * 0.05) {
     int elseHeight[11] = { -1 };
     int oldLeft[11] = { -1 };
     int lastIf = 0;
+    int help = 0;
+
+    setcolor(0);
 
     // trebuie schimbat cum calculeaza functia de inaltime pe if
     // if-urile si for-urile nu se plac deloc
@@ -348,6 +303,7 @@ void createDiagram(blockChain blockVector, int currTop = MAX_HEIGHT * 0.05) {
         if ((blockVector.Block[currIndex].priority <= ifPriority[lastIf]) && inIf) {
             ifEnded = true;
             if (blockVector.Block[currIndex].lineType != 2) { // nu e niciun else
+                help = lastIf;
                 inIf = false;
                 while (ifPriority[lastIf] > blockVector.Block[currIndex].priority) {
                     oldRight[lastIf] = -1;
@@ -385,10 +341,11 @@ void createDiagram(blockChain blockVector, int currTop = MAX_HEIGHT * 0.05) {
             currLeft -= textheight(blockVector.Block[currIndex].rawLine) * (lastPriority - blockVector.Block[currIndex].priority);
         }
         // daca ultimul statement a fost else, inIf = false si current
-        //if (blockVector.Block[currIndex - 1].lineType == 2) elseEnded = true;
+        // if (blockVector.Block[currIndex - 1].lineType == 2) elseEnded = true;
         if (blockVector.Block[currIndex].priority < lastPriority && ifEnded) {
-            currTop -= elseHeight[lastIf];
-            currTop += max(elseHeight[lastIf], ifHeight[lastIf]);
+            cout << "help: " << help << " lastIf: " << lastIf << "elseHeight: " << elseHeight[lastIf] << "ifHeight: " << elseHeight[lastIf];
+            currTop -= elseHeight[help];
+            currTop += max(elseHeight[help], ifHeight[help]);
         }
         ifEnded = false;
         if (blockVector.Block[currIndex].lineType == 1)
@@ -404,6 +361,7 @@ void createDiagram(blockChain blockVector, int currTop = MAX_HEIGHT * 0.05) {
         // daca blocul e for, ii bagam prioritatea si valoarea top in stiva de valori
         if (blockVector.Block[currIndex].lineType == 6 || blockVector.Block[currIndex].lineType == 4) {
             if (blockVector.Block[currIndex].lineType == 4) {
+                drawLoopTestAfterRectangle(blockVector.Block[currIndex], currTop, currLeft, currRight);
                 oldTop[++lastLoop] = drawLoopTestAfter(blockVector.Block[currIndex], currTop, currLeft, currRight);
                 blockType[lastLoop] = 0;
             }
@@ -434,10 +392,11 @@ void createDiagram(blockChain blockVector, int currTop = MAX_HEIGHT * 0.05) {
             lastLoop--;
         }
 
+        cout << '\n' << currLeft << "currLeft" << '\n';
         cout << '\n' << currTop << "current top \n";
         if (blockVector.Block[currIndex].lineType == 1) drawIf(blockVector.Block[currIndex], currTop, currLeft, currRight);
         // PARTE LEGATA DE BLOCURI SIMPLE SI WHILE-URI
-        if (blockVector.Block[currIndex].lineType!=6 && blockVector.Block[currIndex].lineType != 4) drawBlock(blockVector.Block[currIndex],currTop,currLeft,currRight);
+        else drawBlock(blockVector.Block[currIndex],currTop,currLeft,currRight);
     }
     DIAGRAM_SIZE = currTop - originTop;
     printf("\nFinal top:%d, Diagram size: %d\n", currTop, DIAGRAM_SIZE);
