fd887c544b385f8a70186bd1d726730e51436cca
Matei Radu <MateiSR@proton.me>
2024-01-09 15:12:07 +0200

    Reset scroll when exiting main menu

--- Project/main.cpp
+++ Project/main.cpp
@@ -67,7 +67,10 @@ void run()
         swapbuffers();
         ch = getch();
         printf("got character %c - ascii: %d\n", (char)ch, ch);
-        if (ch == 27 || ch == 13) currentPage = mainPage; //running = false; // Esc button to exit
+        if (ch == 27 || ch == 13) {
+            currentPage = mainPage;
+            y = MAX_HEIGHT * 0.05;
+        } //running = false; // Esc button to exit
         else {
             ch = getch();
             printf("got 2nd character %c - ascii: %d\n", (char)ch, ch);
